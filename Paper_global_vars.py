e8452f3a527930f1c444ab74772e033eccecee20
76134681 <761346811@qq.com>
2024-08-09 10:24:04 +0800

    解决元组识别问题

--- Paper_global_vars.py
+++ Paper_global_vars.py
@@ -6,10 +6,10 @@ import torch.nn.functional as F
 class GlobalVars:
     def __init__(self):
         self.num_epochs = 500
-        self.input_size=(3, 224, 224), #astroformer
+        self.input_size=(3, 224, 224) #astroformer
          #'input_size': (3, 32, 32),
         self.max_lr=0.00025
-        self.train_batch_size = 64 // torch.cuda.device_count()
+        self.train_batch_size = 16 // torch.cuda.device_count()
         self.test_batch_size = 64
 
 global_vars = GlobalVars()  # 假设有10个标签
