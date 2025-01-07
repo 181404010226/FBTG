6482596df48ba5b535ce368a67f029533a46dbb2
你的用户名 <>
2025-01-07 09:36:52 +0800

    9785-3579

--- Paper_NeuronBundle.py
+++ Paper_NeuronBundle.py
@@ -19,26 +19,25 @@ class ConvBlock(nn.Module):
         return out
 
 class NeuronBundle(ConvBlock):
-    pass
-    # """单个神经元束，支持循环选择不同大小的卷积核"""
-    # # 添加类变量来追踪当前使用的卷积核索引
-    # current_kernel_index = 0
-    # possible_kernel_sizes = [1, 2, 3, 4, 5, 6, 7]
+    """单个神经元束，支持循环选择不同大小的卷积核"""
+    # 添加类变量来追踪当前使用的卷积核索引
+    current_kernel_index = 0
+    possible_kernel_sizes = [3,5,7,9]
     
-    # def __init__(self, in_channels: int, out_channels: int, **kwargs):
-    #     # 移除传入的 kernel_size
-    #     kernel_size = kwargs.pop('kernel_size', None)
+    def __init__(self, in_channels: int, out_channels: int, **kwargs):
+        # 移除传入的 kernel_size
+        kernel_size = kwargs.pop('kernel_size', None)
         
-    #     # 循环选择卷积核大小
-    #     kwargs['kernel_size'] = self.possible_kernel_sizes[self.current_kernel_index]
-    #     # 更新索引
-    #     NeuronBundle.current_kernel_index = (self.current_kernel_index + 1) % len(self.possible_kernel_sizes)
+        # 循环选择卷积核大小
+        kwargs['kernel_size'] = self.possible_kernel_sizes[self.current_kernel_index]
+        # 更新索引
+        NeuronBundle.current_kernel_index = (self.current_kernel_index + 1) % len(self.possible_kernel_sizes)
         
-    #     # 确保 padding 设置正确
-    #     if 'padding' not in kwargs:
-    #         kwargs['padding'] = kwargs['kernel_size'] // 2
+        # 确保 padding 设置正确
+        if 'padding' not in kwargs:
+            kwargs['padding'] = kwargs['kernel_size'] // 2
             
-    #     super().__init__(in_channels, out_channels, **kwargs)
+        super().__init__(in_channels, out_channels, **kwargs)
 
 class NeuronBundleLayer(nn.Module):
     """神经元束层，包含多个并行的神经元束"""
